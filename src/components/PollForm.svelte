c77f8879fcd3717cf677fbb08fe91c3f29028f76
NoobMaester <joshstilez@gmail.com>
2024-02-27 09:21:52 +0200

    button component

--- src/components/PollForm.svelte
+++ src/components/PollForm.svelte
@@ -1,26 +1,64 @@
 <script>
-    let fields = {question: '', answerA: '', answerB: '',}
+  import Button from "../shared/Button.svelte";
+
+    let fields = {question: '', answerA: '', answerB: ''}
+    let errors = {question:'', answerA: '', answerB: ''}
+    let valid = false;
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(fields)
+        
+        valid=true;
+
+        //validate question
+        if(fields.question.trim().length < 5){
+            valid=false;
+            errors.question = 'Question must be at least 5 characters long'
+        } else {
+            errors.question = ''
+        }
+
+        //validate answerA
+        if(fields.answerA.trim().length < 1){
+            valid=false;
+            errors.answerA = 'Answer A must not be empty'
+        } else {
+            errors.answerA = ''
+        }
+        
+        //validate answerB
+        if(fields.answerB.trim().length < 1){
+            valid=false;
+            errors.answerB = 'Answer B must not be empty'
+        } else {
+            errors.answerB = ''
+        }
+
+        if(valid){
+            alert('Submitted!')
+            console.log('Valid', fields)
+        }
     }
+
 </script>
 
 <form on:submit = {handleSubmit}>
     <div class='form-field'>
         <label for="question">Poll Question</label>
         <input type="text" id="question" bind:value={fields.question}>
+        <div class="error">{errors.question}</div>
     </div>
     <div class='form-field'>
         <label for="answer-a">Answer A</label>
         <input type="text" id="answer-a" bind:value={fields.answerA}>
+        <div class="error">{errors.answerA}</div>
     </div>
     <div class='form-field'>
         <label for="answer-b">Answer B</label>
         <input type="text" id="answer-b" bind:value={fields.answerB}>
+        <div class="error">{errors.answerB}</div>
     </div>
-    <button>Add Poll</button>
+    <Button flat={true}>Add Poll</Button>
 </form>
 
 <style>
@@ -39,5 +77,8 @@
     input{
         width: 100%;
         border-radius: 6px;
+    }
+    .error{
+        color: red;
     }
 </style>
--- src/shared/Button.svelte
+++ src/shared/Button.svelte
@@ -1,11 +1,42 @@
 <script>
-
+    export let type = 'primary';
+    export let flat = false;
+    export let inverse = false;
 </script>
 
-<button>
-    <slot><!-- optional fallback --></slot>
+<button class={type} class:flat = {flat} class:inverse = {inverse}>
+    <slot></slot>
 </button>
 
 <style>
-    
+    button{
+        border: 0;
+        cursor: pointer;
+        border-radius: 6px;
+        padding: 8px 12px;
+        font-weight: bold;
+        box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    }
+    .primary{
+        background: blueviolet;
+        color: #fff;
+    }
+    .secondary{
+        background: #d91b42;
+        color: #fff
+    }
+    .flat{
+        box-shadow: none;
+    }
+    .primary.inverse{
+        color: blueviolet;
+        background: #fff;
+        border: 2px solid blueviolet;
+    }
+    .secondary.inverse{
+        color: #d91b42;
+        background: #fff;
+        border: 2px solid #d91b42;
+    }
+
 </style>
